b02241f65593486eabe05990b226fb151824033b
Zazzao <61016432+Zazzao@users.noreply.github.com>
2026-01-01 18:43:55 -0800

    updates to dialogue UI

--- Assets/System/NPC Dialogue/DialogueUI.cs
+++ Assets/System/NPC Dialogue/DialogueUI.cs
@@ -10,11 +10,15 @@ public class DialogueUI : MonoBehaviour
     public static DialogueUI Instance;
 
     [SerializeField] private GameObject root;
-    [SerializeField] private TextMeshProUGUI nameText;
-    [SerializeField] private TextMeshProUGUI dialogueText;
-    [SerializeField] private Image portraitImage;
+    [SerializeField] private TextMeshProUGUI[] nameText;
+    [SerializeField] private TextMeshProUGUI[] titleText;
+    [SerializeField] private TextMeshProUGUI[] dialogueText;
+    [SerializeField] private Image[] portraitImage;
     [SerializeField] private GameObject leftPanel;
     [SerializeField] private GameObject rightPanel;
+
+    [SerializeField] private float textTypingSpeed = 0.03f;
+    private int boxIndex = 0;
 
     private Coroutine typingRoutine;
     public bool IsTyping { get; private set; }
@@ -28,52 +32,55 @@ public class DialogueUI : MonoBehaviour
     public void Show() => root.SetActive(true);
     public void Hide() => root.SetActive(false);
 
-    public void DisplayLine(DialogueLine line)
-    {
-        if (typingRoutine != null)
-            StopCoroutine(typingRoutine);
+    public void DisplayLine(DialogueLine line){
 
-        nameText.text = line.speakerName;
-        portraitImage.sprite = line.portrait;
+        if (typingRoutine != null) StopCoroutine(typingRoutine);
 
-        //leftPanel.SetActive(line.side == DialogueSide.Left);
-        //rightPanel.SetActive(line.side == DialogueSide.Right);
+        if (line.side == DialogueSide.Right)
+            boxIndex = 0;
+        else
+            boxIndex = 1;
 
-        typingRoutine = StartCoroutine(TypeText(line.message));
+
+
+        nameText[boxIndex].text = line.speakerName;
+        titleText[boxIndex].text = line.title;
+        portraitImage[boxIndex].sprite = line.portrait;
+
+        leftPanel.SetActive(line.side == DialogueSide.Left);
+        rightPanel.SetActive(line.side == DialogueSide.Right);
+
+       
+
+            typingRoutine = StartCoroutine(TypeText(line.message));
     }
 
-    private IEnumerator TypeText(string message)
-    {
+    private IEnumerator TypeText(string message){
         IsTyping = true;
-        dialogueText.text = "";
+        dialogueText[boxIndex].text = "";
 
-       string wrappedText = PreWrapText(message,dialogueText, dialogueText.rectTransform.rect.width);
+        string wrappedText = PreWrapText(message,dialogueText[boxIndex], dialogueText[boxIndex].rectTransform.rect.width);
 
-        foreach (char c in wrappedText)
-        {
-            dialogueText.text += c;
-            yield return new WaitForSeconds(0.03f);
+        foreach (char c in wrappedText){
+            dialogueText[boxIndex].text += c;
+            yield return new WaitForSeconds(textTypingSpeed);
         }
 
         IsTyping = false;
     }
 
-    public void SkipTyping(string message)
-    {
+    public void SkipTyping(string message){
         if (!IsTyping) return;
 
         StopCoroutine(typingRoutine);
-        dialogueText.text = message;
+        dialogueText[boxIndex].text = message;
         IsTyping = false;
     }
 
 
-    private string PreWrapText(
-        string rawText,
-        TextMeshProUGUI tmpText,
-        float maxLineWidth)
+    private string PreWrapText(string rawText, TextMeshProUGUI tmpText, float maxLineWidth)
     {
-        tmpText.enableWordWrapping = false;
+        //tmpText.enableWordWrapping = false;
 
         string[] words = rawText.Split(' ');
         StringBuilder result = new StringBuilder();
@@ -106,6 +113,7 @@ public class DialogueUI : MonoBehaviour
 
         // Append last line
         result.Append(currentLine);
+        tmpText.text = "";
 
         return result.ToString();
     }
