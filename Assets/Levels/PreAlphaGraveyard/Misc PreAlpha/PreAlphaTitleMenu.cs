48e61e112576a056472c845936b0ff1a6c6ef383
Zazzao <61016432+Zazzao@users.noreply.github.com>
2026-01-09 22:40:38 -0800

    update to title menu

--- Assets/Levels/PreAlphaGraveyard/Misc PreAlpha/PreAlphaTitleMenu.cs
+++ Assets/Levels/PreAlphaGraveyard/Misc PreAlpha/PreAlphaTitleMenu.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
+
 
 public class PreAlphaTitleMenu : MonoBehaviour
 {
@@ -14,16 +16,23 @@ public class PreAlphaTitleMenu : MonoBehaviour
     [SerializeField] private int menuItemWt_highlighted = 150;
 
     [Header("Sfx")]
+    [SerializeField] private AudioClip menuBgm;
+    [SerializeField] private AudioClip crowSfx;
     [SerializeField] private AudioClip menuNavSfx;
     private AudioSource audioSource;
 
     [Header("Menu Panels")]
+    [SerializeField] private GameObject mainMenuPanel;
     [SerializeField] private GameObject aboutMenuPanel;
 
+    [Header("Fade In/Out")]
+    [SerializeField] private CanvasGroup fadeOutCanvasGroup;
+    [SerializeField] private CanvasGroup logoCanvasGroup;
+    [SerializeField] private float fadeDuration = 0.35f;
 
     private PlayerInputActions controls;
 
-    private TitleMenuState state = TitleMenuState.MainMenu;
+    private TitleMenuState state = TitleMenuState.Intro;
 
     public enum TitleMenuState { 
         Intro,
@@ -65,9 +74,21 @@ public class PreAlphaTitleMenu : MonoBehaviour
 
     }
 
+    private void Start()
+    {
+        StartCoroutine(MenuIntro());
+    }
+
+
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) Application.Quit(); //debug
+
+
+
+        
+
     }
 
     private void OnUpPerformed(InputAction.CallbackContext context){
@@ -139,5 +160,47 @@ public class PreAlphaTitleMenu : MonoBehaviour
         mainMenuItems[mainMenuIndex].sprite = menuItemBg_highlighted;
         mainMenuItems[mainMenuIndex].rectTransform.sizeDelta = new Vector2(menuItemWt_highlighted, mainMenuItems[mainMenuIndex].rectTransform.sizeDelta.y);
     }
+
+
+    private IEnumerator Fade(CanvasGroup canvasGroup, float targetAlpha)
+    {
+        canvasGroup.blocksRaycasts = true;
+
+        while (!Mathf.Approximately(canvasGroup.alpha, targetAlpha))
+        {
+            canvasGroup.alpha = Mathf.MoveTowards(
+                canvasGroup.alpha,
+                targetAlpha,
+                (1f / fadeDuration) * Time.unscaledDeltaTime
+            );
+
+            yield return null;
+        }
+
+        canvasGroup.alpha = targetAlpha;
+        canvasGroup.blocksRaycasts = targetAlpha > 0f;
+    }
+
+    private IEnumerator MenuIntro() { 
+        
+        yield return new WaitForSeconds(2.0f);
+        audioSource.PlayOneShot(crowSfx);
+        yield return StartCoroutine(Fade(logoCanvasGroup, 1.0f)); // fade in logo
+
+        yield return new WaitForSeconds(2.0f);
+        yield return StartCoroutine(Fade(logoCanvasGroup, 0.0f)); // fade out logo
+
+        audioSource.clip = menuBgm;
+        audioSource.Play();
+        yield return new WaitForSeconds(2.0f);
+        yield return StartCoroutine(Fade(fadeOutCanvasGroup,0.0f)); // fade in
+        yield return new WaitForSeconds(1.0f);
+        Debug.Log("Intro Over");
+        state = TitleMenuState.MainMenu;
+        mainMenuPanel.SetActive(true);
+    
+    }
+
+
 
 }
